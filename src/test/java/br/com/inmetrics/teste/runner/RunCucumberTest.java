db672e4f92a41786a93347e3b71f918d552e5be4
Ivan Schuindt <ivan.schuindt26@hotmail.com>
2020-10-12 04:27:03 -0300

    Implementação e Elaboração dos Testes (não terminada).

--- src/test/java/br/com/inmetrics/teste/runner/RunCucumberTest.java
+++ src/test/java/br/com/inmetrics/teste/runner/RunCucumberTest.java
@@ -5,7 +5,11 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "Feature")
+@CucumberOptions(
+        glue = {"steps"},
+        plugin = {"html:target/cucumber-reports/cucumber-pretty","json:target/json-cucumber-reports/default/cukejson.json",
+                "testng:target/testng-cucumber-reports/cuketestng.xml" },
+        features = {"src/test/resources/features"})
 
 public class RunCucumberTest {
 
